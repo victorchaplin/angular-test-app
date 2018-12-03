a3c34cd71e750c5158524e7fcc269f481a2b56b7
victor <victorchaplin@outlook.com>
2018-12-03 00:36:21 -0200

    initial commit - broken :(

---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomersComponent } from './customers.component';
+
+@NgModule({
+  declarations: [
+    CustomersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CustomersModule { }
