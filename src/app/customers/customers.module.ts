9d80c24b8fa41854c6cb85325743204cb083b8ad
victor <victorchaplin@outlook.com>
2018-12-05 00:08:01 -0200

    adding customers list and fixing app

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomersComponent } from './customers.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
 
 @NgModule({
   declarations: [
-    CustomersComponent
+    CustomersComponent,
+    CustomersListComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CustomersComponent
   ]
 })
 export class CustomersModule { }
