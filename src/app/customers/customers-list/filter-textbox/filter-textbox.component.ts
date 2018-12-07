6f07a5fd26f026a9e85ac22700211491d14723e1
Victor Rocha <victor.rocha@zenvia.com>
2018-12-07 19:11:12 -0200

    adding filter textbox

---
+++ src/app/customers/customers-list/filter-textbox/filter-textbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-textbox',
+  templateUrl: './filter-textbox.component.html',
+  styleUrls: ['./filter-textbox.component.css']
+})
+export class FilterTextboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
+import { FilterTextboxComponent } from './customers-list/filter-textbox/filter-textbox.component';
 
 @NgModule({
   declarations: [
     CustomersComponent,
-    CustomersListComponent
+    CustomersListComponent,
+    FilterTextboxComponent
   ],
   imports: [
     CommonModule
